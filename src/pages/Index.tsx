a6469940fd19ed2006f492894f9abdeb998a1a2d
clayverson06-stack <clayverson06@gmail.com>
2026-09-03 22:28:06 -0300

    feat: usar nova landing page da Dra. Nathalia

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,7 @@
-import { Hero } from "@/components/Hero";
-import { AboutSection } from "@/components/AboutSection";
-import { BenefitsSection } from "@/components/BenefitsSection";
-import { OfferSection } from "@/components/OfferSection";
-import { TestimonialsSection } from "@/components/TestimonialsSection";
-import { FinalCTA } from "@/components/FinalCTA";
+import { LegalLandingPage } from "@/components/LegalLandingPage";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen gradient-subtle">
-      <Hero />
-      <AboutSection />
-      <BenefitsSection />
-      <OfferSection />
-      <TestimonialsSection />
-      <FinalCTA />
-    </div>
-  );
+  return <LegalLandingPage />;
 };
 
 export default Index;
